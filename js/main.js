25a5ff2e5c9d0754f8ed0f9849f22573fccbecda
AndreaRispoli97 <Andrea9715@outlook.it>
2025-03-04 15:23:41 +0100

    add: primo esperimento riuscito

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 console.log("AAAAAA");
+
+const lampElement = document.querySelector(".lamp")
+
+console.log(lampElement);
+
+lampElement.src = '../img/white_lamp.png';
+lampElement.alt = 'Lampadina spenta';
+
+const buttonElement = document.querySelector(".button")
+buttonElement.addEventListener('click', function(){
+    lampElement.src = '../img/yellow_lamp.png';
+})
