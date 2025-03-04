889fe4fddb9f189e5a70b96a801f8d79c6430658
AndreaRispoli97 <Andrea9715@outlook.it>
2025-03-04 15:36:54 +0100

    add: agiiunta immagine all'html

--- js/main.js
+++ js/main.js
@@ -4,10 +4,11 @@ const lampElement = document.querySelector(".lamp")
 
 console.log(lampElement);
 
-lampElement.src = '../img/white_lamp.png';
-lampElement.alt = 'Lampadina spenta';
+// lampElement.src = '../img/white_lamp.png';
+// lampElement.alt = 'Lampadina spenta';
 
 const buttonElement = document.querySelector(".button")
 buttonElement.addEventListener('click', function(){
     lampElement.src = '../img/yellow_lamp.png';
+    lampElement.alt = 'Lampadina accesa'
 })
